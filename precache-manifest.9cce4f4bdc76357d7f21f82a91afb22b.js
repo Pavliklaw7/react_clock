d2e6f8fb766aba38cba87badd6ae52b170f94ed9
Vladyslav Pavlik <pavliklaw7@gmail.com>
2020-10-26 17:06:41 +0200

    Updates

---
+++ precache-manifest.9cce4f4bdc76357d7f21f82a91afb22b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e514151b2c4c62513dad99e1e6a9f72",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3097a45b69e690c6d184",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "33e27156f4d3fa24d47b",
+    "url": "./static/js/2.021d3507.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.021d3507.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3097a45b69e690c6d184",
+    "url": "./static/js/main.74e4b299.chunk.js"
+  },
+  {
+    "revision": "89de80f3985b25002b5f",
+    "url": "./static/js/runtime-main.17b9eccd.js"
+  }
+]);
